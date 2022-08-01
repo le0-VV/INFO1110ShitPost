6a115723c9b363bdf8c84bcf1e20adb66720d2a6
le0-vv <pyleow@icloud.com>
2022-08-01 17:12:28 +1000

    alternative attempt

--- advancedShitposting.sh
+++ advancedShitposting.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s -L  https://raw.githubusercontent.com/le0-VV/INFO1110ShitPost/main/INFO1110AtUSydBeLike.py | python3 -
+wget https://raw.githubusercontent.com/le0-VV/INFO1110ShitPost/main/INFO1110AtUSydBeLike.py
