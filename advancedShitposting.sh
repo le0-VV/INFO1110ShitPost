84d02b9f1ddaa232c3446a41e32a79b38a535415
le0-vv <pyleow@icloud.com>
2022-08-01 17:29:00 +1000

    bugfix attempt

--- advancedShitposting.sh
+++ advancedShitposting.sh
@@ -3,4 +3,4 @@
 # python INFO1110AtUSydBeLike.py
 # rm INFO1110AtUSydBeLike.py
 
-curl -s -L  https://raw.githubusercontent.com/le0-VV/INFO1110ShitPost/main/INFO1110AtUSydBeLike.py >> temp.py; python3 temp.py; rm temp.py
+curl -s -L  https://raw.githubusercontent.com/le0-VV/INFO1110ShitPost/main/INFO1110AtUSydBeLike.py >> temp.py; python3 temp.py #; rm temp.py
