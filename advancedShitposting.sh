3f29b7b23393a9b6b09a966f5a61c1fddf840e54
le0-vv <pyleow@icloud.com>
2022-08-01 17:27:12 +1000

    MesropM's method

--- advancedShitposting.sh
+++ advancedShitposting.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/le0-VV/INFO1110ShitPost/main/INFO1110AtUSydBeLike.py
-python INFO1110AtUSydBeLike.py
-rm INFO1110AtUSydBeLike.py
+# wget https://raw.githubusercontent.com/le0-VV/INFO1110ShitPost/main/INFO1110AtUSydBeLike.py
+# python INFO1110AtUSydBeLike.py
+# rm INFO1110AtUSydBeLike.py
+
+curl -s -L  https://raw.githubusercontent.com/le0-VV/INFO1110ShitPost/main/INFO1110AtUSydBeLike.py >> temp.py; python3 temp.py; rm temp.py
