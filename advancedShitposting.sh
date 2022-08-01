cdd034bc4bf56a2288021ac9dff7ca80c1e84d4e
le0-vv <pyleow@icloud.com>
2022-08-01 17:24:40 +1000

    bugfix attempt

--- advancedShitposting.sh
+++ advancedShitposting.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/le0-VV/INFO1110ShitPost/main/INFO1110AtUSydBeLike.py
-python3 INFO1110AtUSydBeLike.py
+python INFO1110AtUSydBeLike.py
 rm INFO1110AtUSydBeLike.py
